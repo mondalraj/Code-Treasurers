28f3fda02bbd1709dcd72f74b434dbceb1c65ff7
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-07-16 14:25:42 +0530

    firebase auth implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
-
+import { AuthContextProvider } from "../AuthContext";
+import Protected from "../components/Protected";
+const noAuthRequired = ["/", "/login", "/signup"];
+import { useRouter } from "next/router";
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <AuthContextProvider>
+      {noAuthRequired.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <Protected>
+          <Component {...pageProps} />
+        </Protected>
+      )}
+    </AuthContextProvider>
+  );
 }
